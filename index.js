41262eeb05d9a9f5315a472537c547d693a2f603
Ismail <141173099+alcode111@users.noreply.github.com>
2024-02-11 20:20:49 +0100

    Add cheeky message afteruser has submitted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById("modal")
 const closeBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
 
 setTimeout(function() {
     modal.style.display = "inline"
@@ -12,4 +13,12 @@ closeBtn.addEventListener('click', function() {
 
 consentForm.addEventListener('submit', function(e) {
     e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+    </div>
+    `
 })
