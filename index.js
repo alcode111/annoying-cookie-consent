a89856872921a291ec1e9d23b5baa74dc89079b2
Ismail <141173099+alcode111@users.noreply.github.com>
2024-02-11 20:45:04 +0100

    Change paragraph id according to CSS naming conventions

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ consentForm.addEventListener('submit', function(e) {
     modalText.innerHTML = `
     <div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
-    <p id="uploadText">
+    <p id="upload-text">
         Uploading your data to the dark web...
     </p>
     </div>
