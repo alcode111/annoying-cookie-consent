4cf8d65a9d11924eaaf6f1cfd193066080974f45
Ismail <141173099+alcode111@users.noreply.github.com>
2024-02-11 21:46:17 +0100

    Add final cheeky message and some spacing in the code

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ closeBtn.addEventListener('click', function() {
 
 consentForm.addEventListener('submit', function(e) {
     e.preventDefault()
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
@@ -21,7 +22,18 @@ consentForm.addEventListener('submit', function(e) {
     </p>
     </div>
     `
+
     setTimeout(function() {
         document.getElementById("upload-text").innerText = "Making the sale..."
     }, 1500)
+    
+    setTimeout(function() {
+        document.getElementById("modal-inner").innerHTML = `
+        <h2>Thanks <span class="modal-display-name"></span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>
+        ` 
+    }, 3000)
 })
