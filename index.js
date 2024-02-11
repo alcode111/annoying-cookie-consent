e71ce0e7cb3562f53a4ab062051fd7d77b89c016
Ismail <141173099+alcode111@users.noreply.github.com>
2024-02-11 21:50:52 +0100

    Add form submission handling and display full user's name in the modal

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ closeBtn.addEventListener('click', function() {
 consentForm.addEventListener('submit', function(e) {
     e.preventDefault()
 
+    const consentFormData = new FormData(consentForm)
+    const fullName = consentFormData.get('fullName')
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
     <img src="images/loading.svg" class="loading">
@@ -26,10 +29,10 @@ consentForm.addEventListener('submit', function(e) {
     setTimeout(function() {
         document.getElementById("upload-text").innerText = "Making the sale..."
     }, 1500)
-    
+
     setTimeout(function() {
         document.getElementById("modal-inner").innerHTML = `
-        <h2>Thanks <span class="modal-display-name"></span>, you sucker! </h2>
+        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.gif">
